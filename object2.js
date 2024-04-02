67c7e3b06d9e7120ba1737344884199fc11ed75e
ismnurayy <ismnnuray1@gmail.com>
2024-04-02 14:16:50 +0400

    object2

---
+++ object2.js
@@ -0,0 +1,79 @@
+//  let telebeler = [
+//      {
+//          id:1,
+//          Ad: "Nuray",
+//          Soyad: "Ismayilova",
+//          Ortalama: 88,
+//          Xususiyyetler:["sakit","calisqan"],
+//          Hobbi: ["kitab oxumaq", "arasdirma etmek"],
+//          group_id: 11
+//      },
+//      {
+//          id:1,
+//          Ad: "Rena",
+//          Soyad: "Ceferzade",
+//          Ortalama: 94,
+//          Xususiyyetler:["sakit","agilli"],
+//          Hobbi: ["kitab oxumaq", "anime izlemek"],
+//          group_id: 11
+//      },
+//      {
+//          id:1,
+//          Ad: "Gunay",
+//          Soyad: "Babayeva",
+//          Ortalama: 95,
+//          Xususiyyetler:["sozunu deyen","calisqan"],
+//          Hobbi: ["kitab oxumaq", "resm cekmek"],
+//          group_id: 22
+//      },
+//      {
+//          id:1,
+//         Ad: "Nahid",
+//          Soyad: "Allahverdiyev",
+//          Ortalama: 80,
+//          Xususiyyetler:["sozunu deyen","agilli"],
+//          Hobbi: ["futbol oynamaq", "gitar calmaq"],
+//          group_id: 22
+//      },
+//      {
+//          id:1,
+//          Ad: "Meleyke",
+//          Soyad: "Huseynova",
+//          Ortalama: 95,
+//          Xususiyyetler:["sozunu deyen","calisqan"],
+//          Hobbi: [" bir seyleri davamli etmez"],
+//          group_id: 11
+//      }
+//  ]
+//  let groups = [
+//      {
+//          id:11,
+//          Ad:"Komputer Muhendisliyi ||"
+//      },
+//      {
+//          id:22,
+//          Ad:"Komputer Muhendisliyi |"
+//      }
+
+
+//     ]
+
+//     let ad  = prompt("adinizi daxil edin :");
+//     let tel = telebeler.find(telebe => telebe.Ad === ad )
+//     if(tel){
+//         console.log("telebe tapildi"  );
+//         console.log(tel)
+//     }
+//     else{
+//         console.log("default")
+//     }
+//-------------------------------------------------------------------------------
+    // let soz = prompt(" sozu daxil edin :")
+    
+    // for (let i = 0; i < soz.length; i++) {
+    //     console.log(soz.slice(0, i + 1));
+    // }
+    // for (let i = soz.length - 2; i >= 0; i--) {
+    //     console.log(soz.slice(0, i + 1));
+    // }
+//-------------------------------------------------------------------------------
